ae07a43f4475f9d22b4ede7961ffc817d962522d
Victor <vfsilva099@gmail.com>
2022-02-27 11:12:18 -0300

    First commit

---
+++ src/main/java/br/com/mynetflix/MyNetflixApplication.java
@@ -0,0 +1,13 @@
+package br.com.mynetflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNetflixApplication.class, args);
+	}
+
+}
